fd834310da120529ece1edbc72b1ecedc0f3b8da
Smeag <cosmin072@yahoo.com>
2018-06-06 00:02:14 +0300

    Main_Menu+Options

---
+++ Assets/Scripts/MenuButtonInteraction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuButtonInteraction : MonoBehaviour
+{
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Test_landscape");
+    }
+    public void OptionMenu()
+    {
+        SceneManager.LoadScene("Options_Menu");
+    }
+    public void Exit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main_menu");
+    }
+}
